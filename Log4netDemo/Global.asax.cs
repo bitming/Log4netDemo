4eb2efdb00077508494c693eae1c381ba3cc0d84
Bob <mingceng@outlook.com>
2014-05-24 11:59:11 +0800

    log4net的基础配置

--- Log4netDemo/Global.asax.cs
+++ Log4netDemo/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +10,25 @@ namespace Log4netDemo
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        ILogger _logger = new Log4netErrorLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/log4net.config")));
+        }
+
+
+        protected void Application_Error()
+        {
+            var errors = HttpContext.Current.AllErrors;
+
+            foreach (var exception in errors)
+            {
+                _logger.Write(exception.Message);
+            }
         }
     }
 }
